22595ab4b0389c8e9103cb97c19a04483180f4ae
Megan Sampson <msampson@nyc.yearup.org>
2022-11-01 08:04:32 -0400

    completed loop excercise

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Output I love loops 7 times
+
+let phase = "I Love Loops";
+let i ;
+
+for (i = 0; i < 7; i++) {
+   console.log(phase);
+}
---
+++ LoopScripts/while_loops.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let phase = "I love Loops";
+let i = 1; 
+
+while ( i < 8 ) {
+    console.log(phase) ;
+    i++
+
+}
