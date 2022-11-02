c010da9b2b25999e1ca3b854d7672190265aa37a
Megan Sampson <msampson@nyc.yearup.org>
2022-11-02 17:12:29 -0400

    completed Avg Stu

--- ArrayScripts/student_average.js
+++ ArrayScripts/student_average.js
@@ -2,10 +2,29 @@
 
 
 let students = [
-    {name: "Zephaniah", scores: [100, 96, 99, 92]},
-    {name: "Pursalane", scores: [92, 89, 96, 100, 94]},
-    {name: "Siddalee", scores: [86, 72, 92]},
-    {name: "Ian", scores: [98, 84, 89, 100, 100, 76]},
-    {name: "Elisha", scores: [89, 100]},
-    {name: "Ezra", scores: [100, 99, 100, 87]}
-   ];
+    { name: "Zephaniah", scores: [100, 96, 99, 92] },
+    { name: "Pursalane", scores: [92, 89, 96, 100, 94] },
+    { name: "Siddalee", scores: [86, 72, 92] },
+    { name: "Ian", scores: [98, 84, 89, 100, 100, 76] },
+    { name: "Elisha", scores: [89, 100] },
+    { name: "Ezra", scores: [100, 99, 100, 87] }
+];
+
+
+function getAverage(Stu) {
+
+    for(let i = 0; i < Stu.length; i++) {
+     let sum = 0; 
+     
+        for(let w = 0; w < Stu[i].scores.length; w++){
+        
+         sum += Stu[i].scores[w];
+
+        }
+       let average =  sum / Stu[i].scores.length;
+       console.log(average);
+    }
+}
+
+
+getAverage(students);
